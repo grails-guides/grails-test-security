f71f55ec67a1584aa47007b42dd01d156ae90c75
sdelamo <sergio.delamo@softamo.com>
2017-03-22 11:01:22 +0100

    Improve InterceptUrlMap

--- complete/grails-app/conf/application.groovy
+++ complete/grails-app/conf/application.groovy
@@ -16,21 +16,34 @@ grails {
                 className = 'grails.test.security.SecurityRole' // <4>
             }
             interceptUrlMap = [
-                    [pattern: '/', access: ['permitAll']],
-                    [pattern: '/error', access: ['permitAll']],
-                    [pattern: '/index', access: ['permitAll']],
-                    [pattern: '/index.gsp', access: ['permitAll']],
-                    [pattern: '/shutdown', access: ['permitAll']],
-                    [pattern: '/assets/**', access: ['permitAll']],
-                    [pattern: '/**/js/**', access: ['permitAll']],
-                    [pattern: '/**/css/**', access: ['permitAll']],
-                    [pattern: '/**/images/**', access: ['permitAll']],
-                    [pattern: '/**/favicon.ico', access: ['permitAll']],
-                    [pattern: '/login/auth', access: ['ROLE_ANONYMOUS']], // <5>
-                    [pattern: '/announcement/index',  access: ['ROLE_BOSS', 'ROLE_EMPLOYEE']], // <6>
-                    [pattern: '/api/login',  access: ['ROLE_ANONYMOUS']], // <7>
-                    [pattern: '/oauth/access_token',  access: ['ROLE_ANONYMOUS']], // <8>
-                    [pattern: '/api/announcements',  access: ['ROLE_BOSS']] // <9>
+                    [pattern: '/',                      access: ['permitAll']],
+                    [pattern: '/error',                 access: ['permitAll']],
+                    [pattern: '/index',                 access: ['permitAll']],
+                    [pattern: '/index.gsp',             access: ['permitAll']],
+                    [pattern: '/shutdown',              access: ['permitAll']],
+                    [pattern: '/assets/**',             access: ['permitAll']],
+                    [pattern: '/**/js/**',              access: ['permitAll']],
+                    [pattern: '/**/css/**',             access: ['permitAll']],
+                    [pattern: '/**/images/**',          access: ['permitAll']],
+                    [pattern: '/**/favicon.ico',        access: ['permitAll']],
+                    [pattern: '/login/**',              access: ['permitAll']], // <5>
+                    [pattern: '/logout',                access: ['permitAll']],
+                    [pattern: '/logout/**',             access: ['permitAll']],            
+                    [pattern: '/announcement',          access: ['ROLE_BOSS', 'ROLE_EMPLOYEE']],
+                    [pattern: '/announcement/index',    access: ['ROLE_BOSS', 'ROLE_EMPLOYEE']],  // <6>
+                    [pattern: '/announcement/create',   access: ['ROLE_BOSS']],
+                    [pattern: '/announcement/save',     access: ['ROLE_BOSS']],
+                    [pattern: '/announcement/update',   access: ['ROLE_BOSS']],
+                    [pattern: '/announcement/delete/*', access: ['ROLE_BOSS']],
+                    [pattern: '/announcement/edit/*',   access: ['ROLE_BOSS']],            
+                    [pattern: '/announcement/show/*',   access: ['ROLE_BOSS', 'ROLE_EMPLOYEE']],
+                    [pattern: '/api/login',             access: ['ROLE_ANONYMOUS']], // <7>
+                    [pattern: '/oauth/access_token',    access: ['ROLE_ANONYMOUS']], // <8>
+                    [pattern: '/api/announcements',     access: ['ROLE_BOSS'], httpMethod: 'GET'],  // <9>
+                    [pattern: '/api/announcements/*',   access: ['ROLE_BOSS'], httpMethod: 'GET'],
+                    [pattern: '/api/announcements/*',   access: ['ROLE_BOSS'], httpMethod: 'DELETE'],
+                    [pattern: '/api/announcements',     access: ['ROLE_BOSS'], httpMethod: 'POST'],
+                    [pattern: '/api/announcements/*',   access: ['ROLE_BOSS'], httpMethod: 'PUT']
             ]
         }
     }
