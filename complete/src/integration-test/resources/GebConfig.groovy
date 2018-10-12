cd5aa89517d72c271b5b42b397d7c85b434fcfca
sdelamo <sergio.delamo@softamo.com>
2018-10-12 10:02:37 +0200

    Use geb2

--- complete/src/integration-test/resources/GebConfig.groovy
+++ complete/src/integration-test/resources/GebConfig.groovy
@@ -1,12 +1,16 @@
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
-import org.openqa.selenium.htmlunit.HtmlUnitDriver
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.firefox.FirefoxOptions
+
 environments {
-    // run via “./gradlew -Dgeb.env=chrome -Dwebdriver.chrome.driver=/Applications/chromedriver iT”
+
+    // run via “./gradlew -Dgeb.env=chrome iT”
     chrome {
         driver = { new ChromeDriver() }
     }
-    // run via “./gradlew -Dgeb.env=chromeHeadless -Dwebdriver.chrome.driver=/Applications/chromedriver iT”
+
+    // run via “./gradlew -Dgeb.env=chromeHeadless iT”
     chromeHeadless {
         driver = {
             ChromeOptions o = new ChromeOptions()
@@ -14,8 +18,18 @@ environments {
             new ChromeDriver(o)
         }
     }
-    // run via “./gradlew -Dgeb.env=htmlUnit iT”
-    htmlUnit {
-        driver = { new HtmlUnitDriver() }
+
+    // run via “./gradlew -Dgeb.env=firefoxHeadless iT”
+    firefoxHeadless {
+        driver = {
+            FirefoxOptions o = new FirefoxOptions()
+            o.addArguments('-headless')
+            new FirefoxDriver(o)
+        }
+    }
+
+    // run via “./gradlew -Dgeb.env=firefox iT”
+    firefox {
+        driver = { new FirefoxDriver() }
     }
 }
